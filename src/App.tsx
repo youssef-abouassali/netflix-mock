e7dadf6ee6e1cb54230ec6ffeffabc75c05517e4
youssef-abouassali <youssefassali61@gmail.com>
2024-02-24 22:00:31 +0300

    fix project clash

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import NavBar from "./components/NavBar";
+
 function App() {
-  <div>
+  return (
     <div>
-      <s></s>
+      <NavBar />
     </div>
-  </div>;
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+const NavBar = () => {
+  return (
+    <div>
+      <h1 className="text-red-500 text-4xl font-bold cursor-pointer">
+        NETFLIX
+      </h1>
+      <div>
+        <button>ٍSign In</button>
+        <button>Sign Up</button>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
